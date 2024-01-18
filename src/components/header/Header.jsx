25b5a98540db8685f22536ee45b3485cfdf60ded
Gustavo de Sousa Lima <121367654+gustavodesousalima@users.noreply.github.com>
2024-01-18 05:12:46 +0000

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>Sistema de Gerenciamento de Clientes</h1>
+        </header>
+    )
+}
+
+export default Header;
